acde7066a1848a7fcd63cee1d0bf124f346e24d3
amir <amirasfour307@gmail.com>
2025-08-31 02:33:20 +0200

    init

--- market_maker.py
+++ market_maker.py
@@ -30,7 +30,7 @@ class MarketMaker:
             return
 
         asyncio.create_task(self.handle_buy())
-        #asyncio.create_task(self.handle_sell())
+        asyncio.create_task(self.handle_sell())
 
     async def handle_buy(self):
         async with self.buy_lock:
